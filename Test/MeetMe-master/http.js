91b7289a7ef28944f0ecffb5a6a711198f7e8d69
xnipib <elytseerfx@gmail.com>
2022-10-29 14:37:00 -0500

    Create http.js

---
+++ Test/MeetMe-master/http.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+const http = axios.create({
+  baseURL: "http://127.0.0.1:8000/api",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+http.interceptors.request.use(
+  async (config) => {
+    const token = await SecureStore.getItemAsync("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { http };
