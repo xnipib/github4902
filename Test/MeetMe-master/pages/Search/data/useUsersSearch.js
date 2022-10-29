092c36f7dcf2e523c9c6334adee4ae8c45c130d9
xnipib <elytseerfx@gmail.com>
2022-10-29 14:37:55 -0500

    Create useUsersSearch.js

---
+++ Test/MeetMe-master/pages/Search/data/useUsersSearch.js
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { http } from "../../../http";
+
+export function useSearchUsers(keyword) {
+  return useQuery(
+    ["users-search", keyword],
+    async () => {
+      return http
+        .get("/social/search", {
+          params: {
+            email: keyword,
+          },
+        })
+        .then((res) => {
+          return res?.data?.data;
+        })
+        .catch((err) => {
+          return [];
+        });
+    },
+    {
+      staleTime: Infinity,
+      retry: false,
+      initialData: [],
+    }
+  );
+}
