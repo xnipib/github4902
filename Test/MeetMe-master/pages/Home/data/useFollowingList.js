7d89afaf54b4bbf2c110c667e3497a106b6e057b
xnipib <elytseerfx@gmail.com>
2022-10-29 14:37:43 -0500

    Create useFollowingList.js

---
+++ Test/MeetMe-master/pages/Home/data/useFollowingList.js
@@ -0,0 +1,43 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { http } from "../../../http";
+
+export function useFollowingList() {
+  return useQuery(
+    ["following"],
+    async () => {
+      return http.get("/social/followings").then((res) => res?.data?.data);
+    },
+    {
+      staleTime: Infinity,
+      retry: false,
+    }
+  );
+}
+
+export function useUnFollow(opt) {
+  return useMutation(
+    ({ id }) => {
+      return http.delete(`social/follow/${id}`);
+    },
+    {
+      ...opt,
+      onSuccess: (data) => {
+        opt?.onSuccess?.(data);
+      },
+    }
+  );
+}
+
+export function useFollow(opt) {
+  return useMutation(
+    ({ id }) => {
+      return http.post(`social/follow/${id}`, {});
+    },
+    {
+      ...opt,
+      onSuccess: (data) => {
+        opt?.onSuccess?.(data);
+      },
+    }
+  );
+}
