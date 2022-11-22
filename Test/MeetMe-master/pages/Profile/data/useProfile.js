1159f55e03bacbc8b6fc12f4d6e41693afe440b5
Bijay Shrestha <100888243+BeejayStha@users.noreply.github.com>
2022-11-22 14:44:23 -0600

    fixed log out cached data

---
+++ Test/MeetMe-master/pages/Profile/data/useProfile.js
@@ -0,0 +1,51 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { http } from "../../../http";
+
+export function useProfile() {
+  return useQuery(
+    ["profile"],
+    async () => {
+      return http
+        .get("/me")
+        .then((res) => {
+          return res?.data?.data;
+        })
+        .catch((err) => {
+          return {};
+        });
+    },
+    {
+      staleTime: Infinity,
+    }
+  );
+}
+
+export function useToggleVisibility(opt) {
+  return useMutation(
+    (location_visible) => {
+      return http.put(`/location/visibility`, {
+        location_visible,
+      });
+    },
+    {
+      ...opt,
+      onSuccess: (data) => {
+        opt?.onSuccess?.(data);
+      },
+    }
+  );
+}
+
+export function useUpdateLocation(opt) {
+  return useMutation(
+    (location) => {
+      return http.put(`/location`, { location });
+    },
+    {
+      ...opt,
+      onSuccess: (data) => {
+        opt?.onSuccess?.(data);
+      },
+    }
+  );
+}
