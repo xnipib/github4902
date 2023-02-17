458caaff6a4b2f36d181861405607105e9e6d3a2
xnipib <88166304+xnipib@users.noreply.github.com>
2023-02-17 14:33:59 -0600

    Allow the user to choose location on a map

--- Test/MeetMe-master/pages/Home/Home.js
+++ Test/MeetMe-master/pages/Home/Home.js
@@ -1,130 +1,55 @@
 import * as React from "react";
 
-import {
-  View,
-  Text,
-  SafeAreaView,
-  FlatList,
-  Pressable,
-  Alert,
-  RefreshControl,
-} from "react-native";
-import { SimpleLineIcons } from "@expo/vector-icons";
-import Toast from "react-native-root-toast";
+import { View, Text, SafeAreaView, Pressable } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import { useUnFollow, useFollowingList } from "./data/useFollowingList";
 import { LoadingScreen } from "../../components/LoadingScreen";
+import { ListView } from "./components/ListView";
+import { UsersMap } from "./components/UsersMap";
+import { useFollowingList } from "./data/useFollowingList";
 
 export function HomeScreen({ navigation }) {
-  const { data, isLoading, refetch: refetchList } = useFollowingList();
-
-  const { mutate: doUnFollow } = useUnFollow({
-    onSuccess: () => {
-      Toast.show("unfollowed Successfully", {
-        duration: 3000,
-        backgroundColor: "white",
-        textColor: "black",
-        textStyle: {
-          fontSize: 12,
-          color: "black",
-        },
-      });
-      refetchList();
-    },
-    onError: (error) => {
-      Toast.show(error?.response?.data?.message ?? "Server error", {
-        duration: 3000,
-        backgroundColor: "#aa2b30",
-        textColor: "white",
-        textStyle: {
-          fontSize: 12,
-        },
-      });
-    },
-  });
-
-  const createTwoButtonAlert = (user) =>
-    Alert.alert(
-      "Unfollow",
-      `Are you sure you want to unfollow "${user?.name}"?`,
-      [
-        {
-          text: "Cancel",
-          onPress: () => {},
-          style: "cancel",
-        },
-        {
-          text: "OK",
-          onPress: () => {
-            doUnFollow({ id: user?.id });
-          },
-        },
-      ]
-    );
+  const [view, setView] = React.useState("list");
+  const { data, isLoading } = useFollowingList();
 
   if (isLoading) {
     return <LoadingScreen />;
   }
 
+  const currentView = view === "list" ? <ListView /> : <UsersMap />;
+
+  const viewIcons = (icon) => (
+    <View
+      className={`p-1 rounded-full ${view == icon ? "bg-black" : "bg-white"}`}
+    >
+      <Ionicons
+        name={view == icon ? icon : `${icon}-outline`}
+        size={20}
+        color={view == icon ? "white" : "black"}
+      />
+    </View>
+  );
+
   return (
     <SafeAreaView className="bg-gray-50">
       <View className="pb-20">
-        <View>
+        <View className="relative">
           <Text className="text-black text-center text-2xl font-bold mb-2">
             Following
           </Text>
+          <View className="px-6 flex items-end">
+            <Pressable
+              onPress={() => setView(view === "list" ? "map" : "list")}
+            >
+              <View className="flex flex-row gap-2 ">
+                {viewIcons("list")}
+                {viewIcons("map")}
+              </View>
+            </Pressable>
+          </View>
         </View>
         {data?.length > 0 ? (
-          <FlatList
-            className="w-full h-full px-4 mt-4"
-            data={(data ?? []).map((item) => ({
-              ...item,
-              key: item.id,
-            }))}
-            refreshControl={
-              <RefreshControl
-                isRefreshing={isLoading}
-                onRefresh={refetchList}
-                colors={["#000"]} // for android
-                tintColor={"#000"} // for ios
-              />
-            }
-            renderItem={({ item }) => (
-              <View className="shadow-xs bg-white mb-4 px-4 py-3 rounded-lg flex flex-row items-center justify-between">
-                <View className="flex flex-row items-center">
-                  <View className="bg-black h-10 w-10 rounded-lg mr-3 flex items-center justify-center">
-                    <Text className="text-white text-lg font-bold">
-                      {item?.name?.slice(0, 1)?.toUpperCase()}
-                    </Text>
-                  </View>
-                  <View>
-                    <Text className=" font-bold ">{item.name}</Text>
-                    <Text className="text-gray-400 mb-1">{item.email}</Text>
-                    <Text className="text-gray-700 ">
-                      Distance: {!!item.distance ? item.distance + "km" : "NA"}
-                    </Text>
-                  </View>
-                </View>
-                <View className="flex items-center justify-center">
-                  <Pressable
-                    className="bg-white rounded-lg p-3"
-                    onPress={() => {
-                      createTwoButtonAlert(item);
-                    }}
-                    disabled={isLoading}
-                  >
-                    <Text className="text-black text-center font-bold">
-                      <SimpleLineIcons
-                        name="user-unfollow"
-                        size={20}
-                        color="#df4759"
-                      />
-                    </Text>
-                  </Pressable>
-                </View>
-              </View>
-            )}
-          />
+          <View>{currentView}</View>
         ) : (
           <View className="flex h-full mt-12 ">
             <Text className="text-gray-600 text-center text-lg">
