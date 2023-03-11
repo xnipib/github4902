acc0dfd3269bd9590fb51059c463413f8e4cd1aa
Bijay Shrestha <100888243+BeejayStha@users.noreply.github.com>
2023-03-11 11:26:10 -0600

    Add new api to list visited locations

---
+++ Test/social-gathering-api-main/app/Http/Resources/LocationResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LocationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'address' => $this->address,
+            'photo_url' => $this->photo_url,
+        ];
+    }
+}
