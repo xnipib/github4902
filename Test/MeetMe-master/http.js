d0fa31bc4175033f57b5ec8244c7e163c2c5a5b6
xnipib <88166304+xnipib@users.noreply.github.com>
2023-03-11 18:10:24 -0600

    show where to meet a certain user
    mark place as visited

--- Test/MeetMe-master/http.js
+++ Test/MeetMe-master/http.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 const http = axios.create({
-  baseURL:
-    "https://3094-2a02-8109-ae3f-ce38-741a-f583-34a2-b6bd.eu.ngrok.io/api",
+  baseURL: "https://1ce0-196-157-115-98.eu.ngrok.io/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
