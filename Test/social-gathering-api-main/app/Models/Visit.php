bf19da8ba930bab3f3474cf6745fbce71bd7e66c
Bijay Shrestha <100888243+BeejayStha@users.noreply.github.com>
2023-04-08 22:36:05 -0500

    dd Visit resource and implement markLocationVisited API

---
+++ Test/social-gathering-api-main/app/Models/Visit.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Visit extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+
+    public function visitedWith()
+    {
+        return $this->belongsTo(User::class, 'visited_with');
+    }
+
+}
