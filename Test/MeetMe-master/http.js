52d746e3ff30bca1077b166d3dd96fc8d8e82c53
xnipib <88166304+xnipib@users.noreply.github.com>
2023-04-08 22:39:23 -0500

    feat: add my location button

--- Test/MeetMe-master/http.js
+++ Test/MeetMe-master/http.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 const http = axios.create({
-  baseURL: "https://1ce0-196-157-115-98.eu.ngrok.io/api",
+  baseURL:
+    "https://87c9-2a02-8109-ae3f-ce38-c40e-4f81-9f2c-c0c0.eu.ngrok.io/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
