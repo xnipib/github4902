6949d43a3132e1217ff6693a68e544d0ea9bb0b0
xnipib <88166304+xnipib@users.noreply.github.com>
2023-04-27 20:29:02 -0500

    updated api key

--- Test/MeetMe-master/pages/MeetMap/MeetMap.js
+++ Test/MeetMe-master/pages/MeetMap/MeetMap.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import MapViewDirections from "react-native-maps-directions";
 import {
   View,
   StyleSheet,
@@ -7,15 +8,16 @@ import {
   Pressable,
   ActivityIndicator,
 } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import * as Location from "expo-location";
-import { Text } from "react-native-elements";
+import { Icon, Switch, Text } from "react-native-elements";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { openToastr } from "../../utils/Toast";
 import { LoadingScreen } from "../../components/LoadingScreen";
 import { useMeetUser, useNearbyLocations } from "./data/useNearbyLocations";
 import debounce from "lodash.debounce";
+import { useProfile } from "../Profile/data/useProfile";
 
 const initialRegion = {
   latitude: 54.526,
@@ -28,6 +30,29 @@ export function MeetMapScreen({ navigation, route }) {
   const [mapRegion, setMapRegion] = useState(initialRegion);
   const [isMapLoading, setIsMapLoading] = useState(true);
   const [selectedMeetPoint, setSelectedMeetPoint] = useState(null);
+  const [selectedUserLocation, setSelectedUserLocation] = useState(null);
+  const [myLocation, setMyLocation] = useState(null);
+
+  const [isCurrentLocation, setIsCurrentLocation] = useState(true);
+
+  const mapRef = React.useRef(null);
+
+  const { data: profileData } = useProfile();
+
+  const mySavedLocation = useMemo(() => {
+    if (
+      profileData?.location?.coordinates?.[0] &&
+      profileData?.location?.coordinates?.[1]
+    ) {
+      return {
+        latitude: profileData?.location?.coordinates?.[1],
+        longitude: profileData?.location?.coordinates?.[0],
+      };
+    }
+  }, [
+    profileData?.location?.coordinates?.[0],
+    profileData?.location?.coordinates?.[1],
+  ]);
 
   const {
     data: regionLocations,
@@ -51,6 +76,33 @@ export function MeetMapScreen({ navigation, route }) {
     debouncedFilter();
   };
 
+  const setMapCenter = (latitude, longitude) => {
+    setMapRegion({
+      latitude: latitude,
+      longitude: longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  };
+
+  const getCenterLocation = ({
+    firstLatitude,
+    firstLongitude,
+    secondLatitude,
+    secondLongitude,
+  }) => {
+    const firstLatValue = firstLatitude ?? secondLatitude;
+    const firstLngValue = firstLongitude ?? secondLongitude;
+
+    const secondLatValue = secondLatitude ?? firstLatitude;
+    const secondLngValue = secondLongitude ?? firstLongitude;
+
+    return {
+      latitude: (firstLatValue + secondLatValue) / 2,
+      longitude: (firstLngValue + secondLngValue) / 2,
+    };
+  };
+
   const getCurrentPosition = async () => {
     const { status } = await Location.requestForegroundPermissionsAsync();
 
@@ -63,12 +115,10 @@ export function MeetMapScreen({ navigation, route }) {
         user?.location?.coordinates?.[0] &&
         user?.location?.coordinates?.[1]
       ) {
-        setMapRegion({
-          latitude: user?.location?.coordinates?.[1],
-          longitude: user?.location?.coordinates?.[0],
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        });
+        setMapCenter(
+          user?.location?.coordinates?.[1],
+          user?.location?.coordinates?.[0]
+        );
       }
 
       exploreLocation();
@@ -76,6 +126,10 @@ export function MeetMapScreen({ navigation, route }) {
       setIsMapLoading(false);
     } else {
       const location = await Location.getCurrentPositionAsync({});
+      setMyLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
 
       const user = route.params.user;
 
@@ -86,18 +140,22 @@ export function MeetMapScreen({ navigation, route }) {
         user?.location?.coordinates?.[0] &&
         user?.location?.coordinates?.[1]
       ) {
-        centerLatitude =
-          (location.coords.latitude + user?.location?.coordinates?.[1]) / 2;
-        centerLongitude =
-          (location.coords.longitude + user?.location?.coordinates?.[0]) / 2;
+        const { latitude, longitude } = getCenterLocation({
+          firstLatitude: location.coords.latitude,
+          firstLongitude: location.coords.longitude,
+          secondLatitude: user?.location?.coordinates?.[1],
+          secondLongitude: user?.location?.coordinates?.[0],
+        });
+        centerLatitude = latitude;
+        centerLongitude = longitude;
       }
 
-      setMapRegion({
-        latitude: centerLatitude,
-        longitude: centerLongitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+      setSelectedUserLocation({
+        latitude: user?.location?.coordinates?.[1],
+        longitude: user?.location?.coordinates?.[0],
       });
+
+      setMapCenter(centerLatitude, centerLongitude);
 
       setIsMapLoading(false);
     }
@@ -143,6 +201,80 @@ export function MeetMapScreen({ navigation, route }) {
     }
   };
 
+  const coordinates = [
+    {
+      latitude: isCurrentLocation
+        ? myLocation?.latitude
+        : mySavedLocation?.latitude,
+      longitude: isCurrentLocation
+        ? myLocation?.longitude
+        : mySavedLocation?.longitude,
+    },
+    {
+      latitude: selectedUserLocation?.latitude,
+      longitude: selectedUserLocation?.longitude,
+    },
+  ];
+
+  const showDirections =
+    ((myLocation?.latitude && myLocation?.longitude && isCurrentLocation) ||
+      (mySavedLocation?.latitude &&
+        mySavedLocation?.longitude &&
+        !isCurrentLocation)) &&
+    selectedUserLocation?.latitude &&
+    selectedUserLocation?.longitude;
+
+  const directions = useMemo(() => {
+    return (
+      <>
+        <MapViewDirections
+          origin={coordinates[0]}
+          destination={coordinates[1]}
+          apikey={"AIzaSyAduRO4qBTC1Flec9RGCqEhrvfNFwRAJTE"}
+          strokeWidth={4}
+          strokeColor="#111111"
+        />
+
+        <MapView.Marker
+          coordinate={{
+            latitude: coordinates[0].latitude,
+            longitude: coordinates[0].longitude,
+          }}
+          title="Your Location"
+        />
+        <MapView.Marker
+          coordinate={{
+            latitude: coordinates[1].latitude,
+            longitude: coordinates[1].longitude,
+          }}
+          title="Friend Location"
+        />
+      </>
+    );
+  }, [coordinates]);
+
+  const switchLocation = () => {
+    const { latitude, longitude } = getCenterLocation({
+      firstLatitude: !isCurrentLocation
+        ? myLocation?.latitude
+        : mySavedLocation?.latitude,
+      firstLongitude: !isCurrentLocation
+        ? myLocation?.longitude
+        : mySavedLocation?.longitude,
+      secondLatitude: coordinates[1].latitude,
+      secondLongitude: coordinates[1].longitude,
+    });
+
+    mapRef.current?.animateToRegion?.({
+      latitude,
+      longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+
+    setIsCurrentLocation((prev) => !prev);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View className="relative w-full mt-2 mb-1">
@@ -164,37 +296,62 @@ export function MeetMapScreen({ navigation, route }) {
         {isMapLoading ? (
           <LoadingScreen />
         ) : (
-          <MapView
-            style={styles.map}
-            initialRegion={mapRegion}
-            onRegionChange={onRegionChange}
-            showsUserLocation
-            showsPointsOfInterest={false}
-          >
-            {regionLocations?.map((marker, index) => (
+          <>
+            <MapView
+              ref={mapRef}
+              style={styles.map}
+              initialRegion={mapRegion}
+              onRegionChange={onRegionChange}
+              showsUserLocation
+              showsPointsOfInterest={false}
+            >
+              {regionLocations?.map((marker, index) => (
+                <MapView.Marker
+                  key={index}
+                  coordinate={{
+                    latitude: marker.location.latitude,
+                    longitude: marker.location.longitude,
+                  }}
+                  title={marker.name}
+                  description={"Rating:" + marker.rating}
+                  image={require("../../assets/placeMarker.png")}
+                  onPress={() => {
+                    setSelectedMeetPoint(marker);
+                  }}
+                  provider={PROVIDER_GOOGLE}
+                />
+              ))}
+              {showDirections && directions}
               <MapView.Marker
-                key={index}
                 coordinate={{
-                  latitude: marker.location.latitude,
-                  longitude: marker.location.longitude,
+                  latitude: mapRegion.latitude,
+                  longitude: mapRegion.longitude,
                 }}
-                title={marker.name}
-                description={"Rating:" + marker.rating}
-                image={require("../../assets/placeMarker.png")}
-                onPress={() => {
-                  setSelectedMeetPoint(marker);
-                }}
+                title="Meeting Point"
+                description="Your will meet around here"
               />
-            ))}
-            <MapView.Marker
-              coordinate={{
-                latitude: mapRegion.latitude,
-                longitude: mapRegion.longitude,
+            </MapView>
+
+            <Pressable
+              onPress={() => {
+                setIsCurrentLocation((prev) => !prev);
               }}
-              title="You are here"
-              description="Your current location"
-            />
-          </MapView>
+              className="absolute p-2 top-3 right-3 w-48 rounded-full bg-black flex justify-center items-center"
+            >
+              <View className="flex flex-row gap-2 items-center">
+                <Text className="text-white">
+                  {isCurrentLocation ? "Current Location" : "Saved Location"}
+                </Text>
+                <Switch
+                  trackColor={{ false: "#E64C3C", true: "#2563eb" }}
+                  thumbColor={isCurrentLocation ? "#fff" : "#fff"}
+                  ios_backgroundColor="#E64C3C"
+                  onValueChange={switchLocation}
+                  value={isCurrentLocation}
+                />
+              </View>
+            </Pressable>
+          </>
         )}
 
         {!!selectedMeetPoint && (
